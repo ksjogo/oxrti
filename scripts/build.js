4cd738c9edcdb0f1fc40259da0e491c14991d2ef
ksjogo <jogo@kronberger-spiele.de>
2018-07-18 20:46:36 +0200

    try win builds

--- scripts/build.js
+++ scripts/build.js
@@ -6,5 +6,10 @@ replace({
     replacement: "<title>Oxrti - " + (new Date()).toString() + "<\/title>",
     paths: ['./azurestatic/index.html'],
 });
-spawnSync('npx', ["-c webpack --mode 'production'"], { stdio: 'inherit' });
-spawnSync('npx', ["-c webpack --config webpack.functions.js"], { stdio: 'inherit' });
+try {
+    spawnSync('npx', ["-c webpack --mode 'production'"], { stdio: 'inherit' });
+    spawnSync('npx', ["-c webpack --config webpack.functions.js"], { stdio: 'inherit' });
+} catch (e) {
+    console.error(e);
+}
+console.log("finished build!");
